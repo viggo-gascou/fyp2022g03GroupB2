4e43380cad6034db45c849c2f402eb450a0297ae
Gustav Kristensen <gustavkrist@gmail.com>
2022-04-28 13:48:57 +0200

    delete lines print

--- annotate.py
+++ annotate.py
@@ -1,9 +1,14 @@
 from getkey import getkey, keys
+from time import sleep
+from sys import stdout
 
 
 def print_buf(buffer):
     print(" ".join(buffer), end="\r")
 
+
+ERASE_LINE = '\x1b[2K'
+CURSOR_UP_ONE = '\x1b[1A'
 
 with open("manual_annotation.txt", "r") as f:
     lines = [tuple(el.split("@@@ ")) for el in f.read().split("\n")]
@@ -13,29 +18,31 @@ with open("manual_annotation.txt", "r") as f:
         lines[i] = (int(line[0]), line[1])
 
 output = ["idx, irony"]
-buffer = [">", "Not irony", "|", "Irony"]
+buffer = [">", "Not irony", "|", " ", "Irony"]
 
 for idx, tweet in lines:
-    print("\n\n")
     print(tweet)
-    print()
     print("Make a selection")
     print_buf(buffer)
     key = getkey()
     while key != keys.ENTER:
         if key == keys.LEFT:
-            buffer.remove(">")
-            buffer.insert(0, ">")
+            buffer[0] = ">"
+            buffer[3] = " "
             print_buf(buffer)
         elif key == keys.RIGHT:
-            buffer.remove(">")
-            buffer.insert(2, ">")
+            buffer[3] = ">"
+            buffer[0] = " "
             print_buf(buffer)
         key = getkey()
     print(" ".join(buffer))
     index = buffer.index(">")
-    choice = 1 if index == 2 else 0
+    choice = 1 if buffer[3] == ">" else 0
     output.append(f"{idx}, {choice}")
+    # sleep(0.3)
+    for i in range(3):
+        stdout.write(CURSOR_UP_ONE)
+        stdout.write(ERASE_LINE)
 
 
 name = input("Type your name: ")
