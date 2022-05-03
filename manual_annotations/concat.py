0a7f5926b3ea3a59ed678f373262a9677d8d9883
Magnus Simoni Jahn <magnusjahn98@gmail.com>
2022-05-03 15:54:23 +0200

    Fixed error in concat.py and updated annotation results.

--- manual_annotations/concat.py
+++ manual_annotations/concat.py
@@ -3,9 +3,13 @@ import pandas as pd
 
 names = ["Marie", "Frida", "Magnus", "gustav", "viggo"]
 tweet_ids = pd.read_csv(f"annotation_result_{names[0]}.csv")
-tweet_ids = sorted(tweet_ids["idx"])
+tweet_ids = tweet_ids["idx"]
 
 df = pd.DataFrame({"idx": tweet_ids})
+
 for i, name in enumerate(names):
     df[name.title()] = pd.read_csv(f"annotation_result_{names[i]}.csv")[" irony"]
+
+df = df.sort_values(by=['idx'])
+
 df.to_csv("annotation_results.csv")
