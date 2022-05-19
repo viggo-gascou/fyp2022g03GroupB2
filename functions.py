1e1ee6b47227777194fea12f39aef3ab9e2e1b55
gustavkrist <gustavkrist@gmail.com>
2022-05-19 09:28:07 +0000

    deepnote update

---
+++ functions.py
@@ -0,0 +1,26 @@
+import contractions
+import numpy as np
+import regex as re
+
+
+NORMALIZATIONS = {
+    "w/": "with",
+    "w/o": "without"
+}
+
+
+def preprocess(text):
+    link_normalize = re.compile(r"http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+|www\.\w+\.\w{3}")
+    text = re.sub(link_normalize, "@link", text)
+    punct_normalize = re.compile(r"([!,.?]{3})[!,.?]+")
+    text = re.sub(punct_normalize, r"\1", text)
+    tweets = np.array(text.split("\n"))
+    new_tweets = []
+    for i, tweet in enumerate(tweets):
+        word_list = []
+        for word in tweet.split(" "):
+            word = contractions.fix(word.lower())
+            word_list.append(NORMALIZATIONS.get(word, word))
+        tweet = " ".join(word_list)
+        new_tweets.append(tweet)
+    return np.asarray(new_tweets)
