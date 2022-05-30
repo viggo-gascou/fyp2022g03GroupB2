247462313039543520a784af95dfc874de679df7
gustavkrist <gustavkrist@gmail.com>
2022-05-30 12:43:20 +0000

    deepnote update

--- functions.py
+++ functions.py
@@ -26,6 +26,12 @@ def preprocess(text):
     return np.asarray(new_tweets)
 
 def preprocess_tweet(tweet):
+    tweet = re.sub("@\w+", "@user", tweet)
+    tweet = re.sub("\n", " ", tweet)
+    link_normalize = re.compile(r"http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+|www\.\w+\.\w{3}")
+    tweet = re.sub(link_normalize, "@link", tweet)
+    punct_normalize = re.compile(r"([!,.?]{3})[!,.?]+")
+    tweet = re.sub(punct_normalize, r"\1", tweet)
     word_list = []
     for word in tweet.split(" "):
         word = contractions.fix(word.lower())
